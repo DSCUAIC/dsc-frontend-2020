fa97fbad8c339990ea5de3bb912d943f3f71ddea
robbyyt <49028058+robbyyt@users.noreply.github.com>
2020-05-04 19:40:07 +0300

    Sidenav first iteration

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,12 +3,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 const materialModules = [
   MatButtonModule,
   MatInputModule,
   MatCheckboxModule,
   MatDividerModule,
+  MatIconModule,
+  MatSidenavModule
 ];
 
 @NgModule({
