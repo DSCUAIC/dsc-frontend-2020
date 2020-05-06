e74460dcd24d26a840eb08d65a353307d9b16177
RasterDX <rasterplane@hotmail.com>
2020-05-06 09:36:04 -0700

    Implement login

--- src/app/session/store/effects/session.effects.ts
+++ src/app/session/store/effects/session.effects.ts
@@ -1,6 +1,6 @@
-import { createEffect, Actions, ofType } from '@ngrx/effects';
-import { map, mergeMap, catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { createEffect, Actions, ofType, Effect } from '@ngrx/effects';
+import { map, mergeMap, catchError, tap } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
 
 import {
   SessionLoginRequest,
@@ -9,6 +9,7 @@ import {
 } from '../actions';
 import { Injectable } from '@angular/core';
 import { SessionService } from '../../services/session.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class SessionEffects {
@@ -20,7 +21,25 @@ export class SessionEffects {
     ))
   ));
 
+  @Effect({ dispatch: false })
+  LoginSuccess: Observable<any> = this.actions$.pipe(
+    ofType(SessionLoginSuccess),
+    tap((data) => {
+      localStorage.setItem('token', data.payload.token);
+      this.router.navigateByUrl('/');
+    })
+  )
+
+  @Effect({ dispatch: false })
+  LoginFailure: Observable<any> = this.actions$.pipe(
+    ofType(SessionLoginFailure),
+    tap(() => {
+      this.router.navigateByUrl('/login');
+    })
+  )
+
   constructor(
+    private router: Router,
     private actions$: Actions,
     private sessionService: SessionService
   ) {}
